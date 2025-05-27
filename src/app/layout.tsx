0ba18edb2ac4a0a777c113d5b42cb5cf094c51c4
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-05-28 00:16:22 +0900

    eh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased flex flex-col min-h-screen">
         {!isPaid ? (
-          <div className="flex items-center justify-center min-h-screen text-4xl font-bold">
+          <div className="flex items-center justify-center min-h-screen text-4xl font-bold mx-10">
             お金くれるなら、追加機能も含めて公開してあげる
           </div>
         ) : (
